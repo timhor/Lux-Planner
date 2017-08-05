11073f13f10dbc3d2b5c1de44bc7b142284c51c1
Michael Tran <michaelqtran01@gmail.com>
2017-08-05 20:21:40 +1000

    Added barebones server

--- InstaServer/server.py
+++ InstaServer/server.py
@@ -1,17 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def base():
-    return "Hello world"
+    return render_template('index.html')
 
 @app.route('/<auth>')
 def auth_route(auth):
     if auth != 'favicon.ico':
         print(auth)
-    return "Bye world"
+    return render_template('user.html', name = auth)
 
 if __name__ == '__main__':
     app.run(debug=True)
