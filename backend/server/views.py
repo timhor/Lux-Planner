c1e948c4a574f9f5a0aaeca1782befb35c354c3a
Michael Tran <michaelqtran01@gmail.com>
2017-08-11 23:39:27 +1000

    Connected to backend

--- backend/server/views.py
+++ backend/server/views.py
@@ -1,6 +1,58 @@
 from server import app
 from flask import render_template, Response
 import json
+from flask_cors import CORS, cross_origin
+
+CORS(app)
+# logging.getLogger('flask_cors').level = logging.DEBUG
+"""
+from datetime import timedelta
+from flask import make_response, request, current_app
+from functools import update_wrapper
+
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
+"""
 
 @app.route('/')
 @app.route('/index')
@@ -33,7 +85,8 @@ def registered_users():
 	]
 	return render_template('registered-users.html', admin=admin_name, users=fake_regi)
 
-@app.route('/api/hello', methods=['POST'])
+@app.route('/api/hello', methods=['GET'])
+# @crossdomain(origin='*')
 def hello():
 	fake_regi = [
 		{'name': 'James',
@@ -50,5 +103,8 @@ def hello():
 		}
 	]
 	data = json.dumps(fake_regi)
+	print("I am sending!")
+	print(data)
 	return Response(data)
+	# return "Hello world"
 
--- frontend/src/app/michael.service.ts
+++ frontend/src/app/michael.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, Response, RequestOptions } from '@angular/http';
-
-
+import 'rxjs/add/operator/map';
 
 @Injectable()
-
 export class MichaelService {
 
         private server = 'http://127.0.0.1:5000/';
         private httpPoint;
+        private string;
+        private result: Array<any>;
+        private people;
 
     destinations:Array<any>;
     constructor(private http: Http) {
@@ -17,6 +18,10 @@ export class MichaelService {
 
     getServiceData() {
         console.log("Hello from service");
-        //this.http.get();
+        this.string = this.server + "api/hello";
+        console.log(this.string);
+        this.httpPoint.get(this.string).subscribe(response => this.result = response.json());
+        console.log(this.result);
+        //console.log(this.people);
     }
 }
