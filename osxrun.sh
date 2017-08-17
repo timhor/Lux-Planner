6822460c124d0e75791713894b78b5228b28ec93
Michael Tran <michaelqtran01@gmail.com>
2017-08-17 20:49:04 +1000

    Small modifications to osxrun script

--- osxrun.sh
+++ osxrun.sh
@@ -1,18 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 #osascript -e 'tell app "Terminal"
 #   do script ""
 #end tell'
 
+# echo $SHELL
 
-osascript \
-    -e "tell application \"Terminal\"" \
-    -e "tell application \"System Events\" to keystroke \"t\" using {command down}" \
-    -e "do script \"cd backend; workon seng2021; py run.py; deactivate\" in front window" \
-    -e "end tell" > /dev/null
+if "$0" | egrep -q "-bash"; then
+    osascript \
+        -e "tell application \"Terminal\"" \
+        -e "tell application \"System Events\" to keystroke \"t\" using {command down}" \
+        -e "do script \"cd backend; workon seng2021; py run.py; deactivate\" in front window" \
+        -e "end tell" > /dev/null
 
-sleep 5
-osascript \
-    -e "tell application \"Terminal\"" \
-    -e "tell application \"System Events\" to keystroke \"t\" using {command down}" \
-    -e "do script \"cd frontend; ng serve\" in front window" \
-    -e "end tell" > /dev/null
+    sleep 5
+    osascript \
+        -e "tell application \"Terminal\"" \
+        -e "tell application \"System Events\" to keystroke \"t\" using {command down}" \
+        -e "do script \"cd frontend; ng serve\" in front window" \
+        -e "end tell" > /dev/null
+fi
