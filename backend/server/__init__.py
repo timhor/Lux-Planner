e9a21ea408370296169773a8da1f18f03d60556b
Michael Tran <michaelqtran01@gmail.com>
2017-08-29 21:08:28 +1000

    Connection to Flickr successful

--- backend/server/__init__.py
+++ backend/server/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import flickrapi
 
 app = Flask(__name__)
 app.config.from_object('config')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'super-secret-u45bhj35b54b45j456'
 db = SQLAlchemy(app)
+flickr = flickrapi.FlickrAPI('2da548fb2c2e6ed3a55b22e84c6cc550', '800f20038bd86d11', format='parsed-json')
 
 from server import views, models
