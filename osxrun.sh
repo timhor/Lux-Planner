b942d99d5dc936d3590ccc759ca7468fd45d7327
Michael Tran <michaelqtran01@gmail.com>
2017-08-30 21:48:55 +1000

    unbreak script

--- osxrun.sh
+++ osxrun.sh
@@ -5,17 +5,16 @@
 
 # echo $SHELL
 
-if "$0" | egrep -q "-bash"; then
-    osascript \
-        -e "tell application \"Terminal\"" \
-        -e "tell application \"System Events\" to keystroke \"t\" using {command down}" \
-        -e "do script \"cd backend; workon seng2021; py run.py; deactivate\" in front window" \
-        -e "end tell" > /dev/null
 
-    sleep 5
-    osascript \
-        -e "tell application \"Terminal\"" \
-        -e "tell application \"System Events\" to keystroke \"t\" using {command down}" \
-        -e "do script \"cd frontend; ng serve\" in front window" \
-        -e "end tell" > /dev/null
-fi
+osascript \
+    -e "tell application \"Terminal\"" \
+    -e "tell application \"System Events\" to keystroke \"t\" using {command down}" \
+    -e "do script \"cd backend; workon seng2021; py run.py; deactivate\" in front window" \
+    -e "end tell" > /dev/null
+
+sleep 5
+osascript \
+    -e "tell application \"Terminal\"" \
+    -e "tell application \"System Events\" to keystroke \"t\" using {command down}" \
+    -e "do script \"cd frontend; ng serve\" in front window" \
+    -e "end tell" > /dev/null
