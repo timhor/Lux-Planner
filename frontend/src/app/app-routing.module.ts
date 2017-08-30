d96382066f60297209023b5cbf3b431e3679cf2d
Tim Hor <timothy.hor@outlook.com>
2017-08-30 22:15:10 +1000

    Add sign-up component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 import { DestinationComponent } from "./destination/destination.component";
 
 const routes: Routes = [
@@ -14,6 +15,10 @@ const routes: Routes = [
     {
         path: 'login',
         component: LoginComponent
+    },
+    {
+        path: 'signup',
+        component: SignupComponent
     },
     {
         path: 'destination',
