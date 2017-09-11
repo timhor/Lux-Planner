5d8b9fa034114da0453ce62792c13dc489d6ddb4
VintonyPadmadiredja <vintony.padmadiredja@student.unsw.edu.au>
2017-09-12 00:50:43 +1000

    Add components to Sign Up

---
+++ frontend/src/app/signup/account.ts
@@ -0,0 +1,10 @@
+export class Account {
+   
+   constructor(
+      public username: string,
+      public password: string,
+      public confirmPassword: string,
+      public email: string
+   ) { }
+
+}
--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { Account } from './account';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {}
+export class SignupComponent {
+  account = new Account('','','','');
+
+  submitted = false;
+
+  onSubmit() {
+    this.submitted = true;
+    
+    //Add stuff here to send account details to backend
+    //And redirect to --> routerLink="/login"
+
+  }
+
+  // For debugging
+  get diagnostic() { return JSON.stringify(this.account); }
+}
