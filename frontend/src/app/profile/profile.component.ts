32d35ca5ffeb1178fc89ba1301220cd30e2099be
VintonyPadmadiredja <vintony.padmadiredja@student.unsw.edu.au>
2017-09-13 19:31:32 +1000

    Add Models to Profile

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
+  public avatar;
+  public username;
+  public email;
+  public dateJoined;
+  public journeys;
   constructor() { }
 
   ngOnInit() {
+    //fetch account details from backend
+    //TODO: replace literals with details
+    this.avatar = "https://dummyimage.com/250x250/000/fff";
+    this.username = "Username";
+    this.email = "userEmail@mail.com";
+    this.dateJoined = "dd/mm/yyyy";
+    this.journeys = ['Journey1', 'Journey2', 'Journey3'];
   }
 
 }
