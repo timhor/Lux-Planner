b4ca204f14f4d4097c4da60f9e2ceef6c3728b23
VintonyPadmadiredja <vintony.padmadiredja@student.unsw.edu.au>
2017-09-13 19:16:56 +1000

    Add Models to Login

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -7,12 +7,31 @@ import { LoggedInService } from '../loggedIn.service';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  public username;
+  public password;
+  public isChecked = false; 
+  public incorrectCredentials = false;
+
+  submitted = false;
   
+  setChecked() {
+    this.isChecked = !this.isChecked;
+  }
+  onSubmit() {
+    this.submitted = true;
+    
+    //Add stuff here to send account details to backend
+    //this.username, this.password, this.isChecked
+    //And redirect to --> routerLink="/dashboard OR /journey"
+
+  }
+
   constructor(private loggedInService: LoggedInService) {
   }
 
   setLoggedIn() {
     this.loggedInService.loggedIn = true;
-    console.log(this.loggedInService.loggedIn);
+    console.log("Logged In=" + this.loggedInService.loggedIn);
   }
+  get diagnostic() { return("Username: " + this.username + " Password: " + this.password + " Check: " + this.isChecked); }
 }
