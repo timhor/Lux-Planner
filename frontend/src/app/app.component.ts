9e8777fbf984deefd6bad7988d9e98f9e7424027
Michael Tran <michaelqtran01@gmail.com>
2017-09-17 21:46:06 +1000

    Removed redunant thing

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -13,16 +13,7 @@ export class AppComponent {
   public title = 'LUX: Holiday Planner';
   public username: String = 'Username';
 
-  constructor(private loggedInService: LoggedInService, private router: Router, ) {
-    try {
-        let jwtHelper: JwtHelper = new JwtHelper();    
-        let token: string = localStorage.getItem('id_token');
-        let decoded = jwtHelper.decodeToken(token);
-        this.username = decoded.identity[1];
-    } catch (e) {
-        // Do nothing, user not logged in
-    }
-}
+  constructor(private loggedInService: LoggedInService, private router: Router, ) {}
 
   public isLoggedIn() {
     try {
