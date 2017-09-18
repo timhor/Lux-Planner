9464ed58a76ffeaf19a13d2c3cbee4ef3a256b70
VintonyPadmadiredja <vintony.padmadiredja@student.unsw.edu.au>
2017-09-19 00:07:16 +1000

    Add temporary frontend implementation for Dashboard

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -9,20 +9,31 @@ import { ItineraryComponent } from '../itinerary/itinerary.component';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  public currJourney = "Journey 1";
-  public stops: string[] = ['Tokyo', 'Hong Kong', 'Singapore'];
-  public activeIndex = 0;
+  public journeyName: string = 'Journey1'; //set as empty for all once backend works
+  public stops: string[] = ['Tokyo', 'Hong Kong', 'Singapore']; 
+  public allJourneys: string[] = ['Journey1', 'Journey2', 'Journey3'];
+  public activeJourneyIndex = 0;
+  public activeStopIndex = 0;
   public aboutText: string = "Loading Information...";
   public connService: ConnectionService;
 
   constructor( _connectionService: ConnectionService) {
     this.connService = _connectionService;
+    // this.connService.getProtectedData('api/get_all_journeys').subscribe(
+    //   res => {
+    //       this.activeJourneyIndex = res.active_journey;
+    //       this.allJourneys = res.journeys;
+    //       this.journeyName = res.journeys[this.activeIndex].journey_name;
+    //       this.stops = res.journeys[this.activeIndex].stops;
+    //   }        
+    // );
+
     this.connService.getServiceData('api/stop_information/?stop='+ this.getCurrStop()).subscribe(
       res => {
           this.aboutText = res.info; 
           console.log("About text is " + this.aboutText);   
       }        
-  );
+    );
   }
 
   ngOnInit() {
@@ -30,7 +41,43 @@ export class DashboardComponent implements OnInit {
 
   getCurrStop () {
     //temporarily. Should something check which is active
-    return this.stops[this.activeIndex];
+    return this.stops[this.activeStopIndex];
+  }
+
+  setActiveJourney(name:string) {
+    // console.log(name);
+    if (name === this.journeyName) return;
+    this.journeyName = name;
+    
+    for (let i=0; i < this.allJourneys.length; i++) {
+      // TODO replace with allJourneys[i].journey_name
+      if (this.allJourneys[i] === name) {
+        //this.stops = this.allJourneys[i].stops; <-- Uncomment this after
+        this.activeJourneyIndex = i;
+        // console.log(this.activeIndex);
+        break;
+      }
+    }
+    this.connService.getServiceData('api/stop_information/?stop='+ this.getCurrStop()).subscribe(
+      res => {
+          this.aboutText = res.info; 
+          console.log("About text is " + this.aboutText);   
+      }        
+    );
+  }
+
+  setActiveStop(stop:string) {
+    for (let i=0; i < this.stops.length; i++) {
+      if (stop === this.stops[i]) {
+        this.activeStopIndex = i;
+      }
+    }
+    this.connService.getServiceData('api/stop_information/?stop='+ this.getCurrStop()).subscribe(
+      res => {
+          this.aboutText = res.info; 
+          console.log("About text is " + this.aboutText);   
+      }        
+    );
   }
 
 }
