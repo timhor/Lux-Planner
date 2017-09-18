05e98ca27a52e532fabd7587e9e4e62d504549d8
Tim Hor <timothy.hor@outlook.com>
2017-09-19 00:29:05 +1000

    Restore original placeholder text for search bar on homepage

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -12,6 +12,11 @@ import { SearchService } from '../search.service'
 export class HomeComponent {
   constructor(private searchService: SearchService) {}
 
+  ngOnInit() {
+    let mainSearchBar = <HTMLInputElement>document.getElementById("mainSearch").children[0];
+    mainSearchBar.placeholder = "Search for location..."
+  }
+
   sendQuery() {
     console.log((<HTMLInputElement>document.getElementById("mainSearch")).value);
     this.searchService.query =(<HTMLInputElement>document.getElementById("mainSearch")).value;
