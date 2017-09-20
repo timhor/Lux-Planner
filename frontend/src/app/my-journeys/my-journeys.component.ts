2776e1b840278542cdca7f2a7c7e1e70411a1464
vintony.padmadiredja@student.unsw.edu.au <vintony.padmadiredja@student.unsw.edu.au>
2017-09-20 22:16:33 +1000

    Make My-Journeys Dynamic

--- frontend/src/app/my-journeys/my-journeys.component.ts
+++ frontend/src/app/my-journeys/my-journeys.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ConnectionService } from '../connection/connection.service';
 
 @Component({
   selector: 'app-my-journeys',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['../app.component.css', './my-journeys.component.css']
 })
 export class MyJourneysComponent implements OnInit {
-
-  constructor() { }
+  public allJourneys = [{'journey_name': 'Journey', 'stops': []}];
+  public connService: ConnectionService;
+  constructor( _connectionService: ConnectionService) {
+    this.connService = _connectionService; 
+  }
 
   ngOnInit() {
+    this.connService.getProtectedData('api/get_all_journeys').subscribe(
+      res => {
+          this.allJourneys = res.journeys;
+          console.log('Success getting journeys');
+          console.log(res);            
+      },
+      (error) => {console.log(`could not connect ${error}`)}
+    ); 
   }
 
 }
