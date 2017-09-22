2535543c3469160b596b25e761c56917be050064
Soham Patel <z5115401@student.unsw.edu.au>
2017-09-23 01:39:51 +1000

    Set up basis of journey overview in dashboard

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -25,31 +25,33 @@ export class DashboardComponent implements OnInit {
   public mapUrl: string = "Nothing";
   public loggedInService: LoggedInService;
   private toRefresh:boolean = false;  
+  private firstLoad:boolean;
 
   constructor( _connectionService: ConnectionService, public sanitizer: DomSanitizer, _loggedinService: LoggedInService, public router: Router) {
     this.connService = _connectionService;
     this.loggedInService = _loggedinService;
+    this.firstLoad = true;
 
     this.connService.getProtectedData('api/get_all_journeys').subscribe(
         res => {
             this.activeJourneyIndex = res.active_journey;
             this.allJourneys = res.journeys;
             this.journeyName = res.journeys[this.activeJourneyIndex].journey_name;
-            this.stops = res.journeys[this.activeStopIndex].stops;
-            
+            this.stops = res.journeys[this.activeStopIndex].stops;            
             this.connService.getServiceData('api/stop_information/?stop='+ this.getCurrStop()).subscribe(
                 res => {
                     this.aboutText = res.info; 
                 }                
             );
             this.setUrls(this.getCurrStop());     // Refresh the Map   
+            this.firstLoad = false;                            
         },
         (error) => {console.log(`could not connect ${error}`)}
     ); 
-    this.aboutText = this.regInfo(this.aboutText);   
+    this.aboutText = this.regInfo(this.aboutText); 
   }
-
-  ngOnInit() {
+  
+  ngOnInit() {    
     if (!this.loggedInService.loggedIn()) {
       this.router.navigate(['/login']);
     }
@@ -60,6 +62,9 @@ export class DashboardComponent implements OnInit {
   }
 
   setActiveJourney(name:string) {
+    if (!this.firstLoad){
+      this.firstLoad = true;
+    }                                
     if (name === this.journeyName) return;
     this.journeyName = name;
     
@@ -76,11 +81,12 @@ export class DashboardComponent implements OnInit {
           this.aboutText = res.info; 
       }        
     );
+    
     this.setUrls(this.getCurrStop());  // Refresh the Map
   }
 
   setActiveStop(stop:string) {
-    
+    this.firstLoad = false;
     for (let i=0; i < this.stops.length; i++) {
       if (stop === this.stops[i].name) {
         this.activeStopIndex = i;
@@ -104,7 +110,10 @@ export class DashboardComponent implements OnInit {
 
   setUrls(stopName:string){
     // this.weatherUrl = "https://forecast.io/embed/#lat=" + this.stops[this.activeStopIndex].lat + "&lon=" + this.stops[this.activeStopIndex].lng + "&units=uk&color=#000037";
-    this.mapUrl = "https://www.google.com/maps/embed/v1/place?key=AIzaSyAWhdBjPKjj_DNstBfp3i65VTtCeEzucyc&q=" + stopName + " City";  
+    this.mapUrl = "https://www.google.com/maps/embed/v1/place?key=AIzaSyAWhdBjPKjj_DNstBfp3i65VTtCeEzucyc&q=" + stopName + " City";
+    if (this.firstLoad){
+      this.activeStopIndex = -1; 
+    }
   }
 
   regStop(string) {
