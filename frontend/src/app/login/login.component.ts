05463aa8f00b7d3d47992565340fd379a1cd7daf
VintonyPadmadiredja <vintony.padmadiredja@student.unsw.edu.au>
2017-10-05 20:18:10 +1100

    Add notification popup

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoggedInService } from '../loggedIn.service';
-
+import { NotificationsService } from 'angular2-notifications';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -12,6 +12,8 @@ export class LoginComponent {
   public password;
   public isChecked = true; 
   public incorrectCredentials = false;
+
+  constructor(private loggedInService: LoggedInService, public router: Router, private notification: NotificationsService) {}
   
   setChecked() {
     this.isChecked = !this.isChecked;
@@ -22,7 +24,10 @@ export class LoginComponent {
         (data) => {
             let token = data.access_token;
             localStorage.setItem('id_token', token);  // 'id_token' is the default location AuthHTTP looks for
-            this.router.navigate(['/dashboard'])
+            this.notify();
+            this.router.navigate(['/dashboard']);
+            window.scrollTo(0,0);
+
         },
         (error) => {
             console.log(`Sumting wong: ${error}`);
@@ -30,11 +35,17 @@ export class LoginComponent {
         }
     )
   }
-
-  constructor(private loggedInService: LoggedInService, public router: Router) {}
   
   setLoggedIn() {
     console.log("Logged In=" + this.loggedInService.loggedIn);
   }
+
+  notify() {
+    this.notification.success(
+      this.username,
+      "Logged in successfully",
+    );
+  }
+
   get diagnostic() { return("Username: " + this.username + " Password: " + this.password + " Check: " + this.isChecked); }
 }
