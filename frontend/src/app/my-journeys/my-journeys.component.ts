514703f420b478d57d6e2398b39a79d1c91fb1d5
vintony.padmadiredja@student.unsw.edu.au <vintony.padmadiredja@student.unsw.edu.au>
2017-10-06 17:20:48 +1100

    Changes to notifications and add notification to sign out

--- frontend/src/app/my-journeys/my-journeys.component.ts
+++ frontend/src/app/my-journeys/my-journeys.component.ts
@@ -100,7 +100,6 @@ export class MyJourneysComponent implements OnInit {
       "No Exisiting Journeys",
       "Redirecting...",
       {
-        timeOut: 1000,
         showProgressBar: true
       }
     );
