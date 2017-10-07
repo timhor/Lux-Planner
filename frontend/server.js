e702b94fba10f5e3d6715085316ed0f404b21384
Michael Tran <michaelqtran01@gmail.com>
2017-10-07 20:58:02 +1100

    added gunicorn for backend hosting

--- frontend/server.js
+++ frontend/server.js
@@ -1,9 +1,11 @@
 // server.js
 const express = require('express');
+const fallback = require('express-history-api-fallback');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
+app.use(fallback(__dirname + '/dist/index.html'))
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
