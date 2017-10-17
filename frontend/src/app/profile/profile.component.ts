c475d25468e161ed3a079a37456c8fc2282c5ef7
Tim Hor <timothy.hor@outlook.com>
2017-10-17 22:11:49 +1100

    Use global well class for Profile

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -6,7 +6,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['../app.component.css', './profile.component.css']
 })
 export class ProfileComponent implements OnInit {
   public avatar: string;
