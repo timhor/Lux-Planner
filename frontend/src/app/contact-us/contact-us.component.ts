e1e96e879231558e63b6878fe356b37c31c61efb
Tim Hor <timothy.hor@outlook.com>
2017-10-17 21:52:43 +1100

    Use global well class for Contact Us

--- frontend/src/app/contact-us/contact-us.component.ts
+++ frontend/src/app/contact-us/contact-us.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
-  styleUrls: ['./contact-us.component.css']
+  styleUrls: ['../app.component.css', './contact-us.component.css']
 })
 export class ContactUsComponent implements OnInit {
 
