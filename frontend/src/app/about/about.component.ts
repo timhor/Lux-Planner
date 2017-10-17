56f9947ea825f7df35394fa72f0704b7ac004fe9
Tim Hor <timothy.hor@outlook.com>
2017-10-17 21:44:03 +1100

    Use global well class for About

--- frontend/src/app/about/about.component.ts
+++ frontend/src/app/about/about.component.ts
@@ -3,13 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['../app.component.css', './about.component.css']
 })
-export class AboutComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class AboutComponent {}
