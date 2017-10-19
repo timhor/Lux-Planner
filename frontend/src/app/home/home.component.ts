366655f04b7248edce38e87485d2c070090c1fa3
Tim Hor <timothy.hor@outlook.com>
2017-10-19 23:04:48 +1100

    Change button on home page to Sign Up if not logged in

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -3,15 +3,20 @@ import {} from '@types/googlemaps';
 import { Component, ViewChild, ElementRef, NgZone } from '@angular/core';
 import { SearchService } from '../search/search.service'
 import { ConnectionService } from '../connection.service';
+import { LoggedInService } from '../loggedIn.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
+  styleUrls: ['../app.component.css', './home.component.css'],
 })
 
 export class HomeComponent {
-  constructor(private searchService: SearchService, private connService: ConnectionService) {
+  constructor(
+    private searchService: SearchService,
+    private connService: ConnectionService,
+    private loggedInService: LoggedInService
+  ) {
     this.connService.getServiceData('api/stop_information').subscribe();
   }
 
