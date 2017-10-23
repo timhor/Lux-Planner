982be881098c736b695ac3934ae343d8d5ad6a0e
Michael Tran <michaelqtran01@gmail.com>
2017-10-23 20:14:12 +1100

    Added comments to what I was planning to add

--- frontend/src/app/journey.service.ts
+++ frontend/src/app/journey.service.ts
@@ -4,5 +4,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class JourneyService {
-   public activeJourneyIndex = 0;
+   public activeJourneyIndex: number = 0;
+//    public addNewStop: boolean;
 }
